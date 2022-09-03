d650afecc6e0dad093e49138d57b1570e59b6f08
Jose89fcb <60312427+jose89fcb@users.noreply.github.com>
2022-09-03 19:08:27 +0200

    Add files via upload

---
+++ RobarAvatar.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='!', description="ayuda bot") #Comando
+bot.remove_command("help") # Borra el comando por defecto !help
+
+
+@bot.command()
+async def avatar(ctx, *, member:discord.Member =None):
+    if member == None:
+            member = ctx.message.author
+
+    await ctx.send(member.avatar_url)
+
+
+
+
+
+
+@bot.event
+async def on_ready():
+    print("BOT listo!")
+
+
+ 
+ 
+ 
+bot.run('')    #Crea un bot e un Token: https://discord.com/developers/applications
